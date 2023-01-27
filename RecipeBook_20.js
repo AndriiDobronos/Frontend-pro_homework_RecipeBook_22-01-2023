66524a368ad6006e068063a8f72dcdf9a8949901
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-27 17:48:42 +0200

    corrected homework

--- RecipeBook_20.js
+++ RecipeBook_20.js
@@ -50,98 +50,84 @@ class Recipe {
         this.name = name
         this.listOfIngredients = listOfIngredients
         this.descriptionOfPreparation = descriptionOfPreparation
-        this.cookingTime = cookingTime
+        this.cookingTime = parseFloat(cookingTime)
     }
     isValidProperties() {
-        if (this.name?.trim() && this.listOfIngredients?.trim() &&
+        if (this.name?.trim() && this.listOfIngredients?.length  &&
             this.descriptionOfPreparation?.trim() && this.cookingTime > 0) {
             return true
         }
         return false
     }
-
 }
 
 class RecipeBook {
     arrayOfRecipes = []
-    addingRecipes(recipes) {
-        const isValidProperties = []
-        for (let d = 0; d < recipes.length; d++) {
-            isValidProperties.push(recipes[d].isValidProperties())
-            if (isValidProperties[d]) {
-                this.arrayOfRecipes.push(recipes[d])
-            }
+
+    addingRecipes(recipe) {
+        const isValidProperties = recipe.isValidProperties()
+        if (isValidProperties) {
+            this.arrayOfRecipes.push(recipe)
         }
         return this.arrayOfRecipes
     }
 
-    arrayRecipeWhichTimeLimit = []
-    isValidTime(cookingTimeLimit, recipes) {
-        const isValidProperties = []
-        for (let i = 0; i < recipes.length ; i++) {
-            isValidProperties.push(recipes[i].isValidProperties())
-            if (recipes[i].cookingTime > 0 &&
-                recipes[i].cookingTime < cookingTimeLimit &&
-                isValidProperties[i]) {
-                this.arrayRecipeWhichTimeLimit.push(recipes[i])
+    isValidTime(cookingTimeLimit) {
+        this.arrayRecipeWhichTimeLimit = []
+        for (let i = 0; i < this.arrayOfRecipes.length; i++) {
+            const timeCook = parseFloat(this.arrayOfRecipes[i].cookingTime)
+            if (timeCook > 0 &&
+                timeCook <= parseFloat(cookingTimeLimit)) {
+                this.arrayRecipeWhichTimeLimit.push(this.arrayOfRecipes[i])
             }
         }
-       return this.arrayRecipeWhichTimeLimit
     }
 
-    arrayRecipeNecessaryIngredients = []
-    listOfIngredients = recipes.listOfIngredients
-    isNecessaryIngredients(necessaryIngredients, recipes) {
-        const isValidProperties = []
-        for (let j = 0; j < recipes.length ; j++) {
-            isValidProperties.push(recipes[j].isValidProperties())
-            if (recipes[j].listOfIngredients.includes(necessaryIngredients[0]) &&
-                recipes[j].listOfIngredients.includes(necessaryIngredients[1]) &&
-                isValidProperties[j]) {
-                this.arrayRecipeNecessaryIngredients.push(recipes[j])
-            }
-        }
-        return this.arrayRecipeNecessaryIngredients
+    isNecessaryIngredients(necessaryIngredients) {
+        this.arrayRecipeNecessaryIngredients = this.arrayOfRecipes.filter((recipe) => {
+            return necessaryIngredients.every(ingredient => recipe.listOfIngredients.includes(ingredient))
+        })
     }
 }
 
-const recipe1 = new Recipe("Lorem1","meat,cheese,томати",
+
+const recipe1 = new Recipe("Lorem1",["meat","cheese","томати"],
     "to mixe,to bake",60)
-const recipe2 = new Recipe("Lorem2","meat,морква,томати",
+const recipe2 = new Recipe("Lorem2",["meat","морква","томати"],
     "to mixe,to bake",120)
-const recipe3 = new Recipe("Lorem3","картопля,salt,onions,томати",
-    "to mixe,to bake",30)
-const recipe4 = new Recipe("Lorem4","картопля,морква,томати",
+const recipe3 = new Recipe("Lorem3",["картопля","salt","onions","томати"],
+    "to mixe,to bake","30minutes")
+const recipe4 = new Recipe("Lorem4",["картопля","морква","томати"],
     "to mixe,to bake",50)
-const recipe5 = new Recipe("Lorem5","cucumber,onions,томати",
+const recipe5 = new Recipe("Lorem5",["cucumber","onions","томати"],
     "to mixe,to add butter",55)
-const recipe6 = new Recipe("Lorem6","cucumber,onions,томати,cheese",
+const recipe6 = new Recipe("Lorem6",["cucumber","onions","томати","cheese"],
     "to mixe,to add salt",25)
-const recipe7 = new Recipe("Lorem7","cucumber,onions,томати,cheese",
+const recipe7 = new Recipe("Lorem7",["cucumber","onions","томати","cheese"],
     "to mixe,to add salt",0)
-const recipes = [recipe1,recipe2,recipe3,recipe4,recipe5,recipe6,recipe7]
+
 const recipeBook = new RecipeBook()
-
-recipeBook.addingRecipes(recipes)
+recipeBook.addingRecipes(recipe1)
+recipeBook.addingRecipes(recipe2)
+recipeBook.addingRecipes(recipe3)
+recipeBook.addingRecipes(recipe4)
+recipeBook.addingRecipes(recipe5)
+recipeBook.addingRecipes(recipe6)
+recipeBook.addingRecipes(recipe7)
 console.log(recipeBook.arrayOfRecipes)
-//recipeBook.isValidTime(61,recipes)
-//console.log(recipeBook.arrayRecipeWhichTimeLimit)
-//recipeBook.isNecessaryIngredients(["картопля","томати"], recipes)
-//console.log(recipeBook.arrayRecipeNecessaryIngredients)
 
-recipeBook.isValidTime(60,recipes)
+recipeBook.isValidTime("60minutes")
 const arrayRecipeWhichTimesLimit = recipeBook.arrayRecipeWhichTimeLimit
 let listName = ""
 for (let c = 0; c < arrayRecipeWhichTimesLimit.length ; c++) {
-    listName = listName + `${arrayRecipeWhichTimesLimit[c].name}\n`
+    listName = listName + `${arrayRecipeWhichTimesLimit[c].name} : cooking time : ${arrayRecipeWhichTimesLimit[c].cookingTime}\n`
 }
 console.log(`The name recipes which time limit : 60 \n ${listName}`)
 
-recipeBook.isNecessaryIngredients(["картопля","морква"], recipes)
+recipeBook.isNecessaryIngredients(["картопля","морква"])
 const arrayRecipesNecessaryIngredients = recipeBook.arrayRecipeNecessaryIngredients
 let listNameNecessaryIngredients = ""
 for (let s = 0; s < arrayRecipesNecessaryIngredients.length ; s++) {
-    listNameNecessaryIngredients = listNameNecessaryIngredients + `${arrayRecipesNecessaryIngredients[s].name}\n`
+    listNameNecessaryIngredients = listNameNecessaryIngredients + `${arrayRecipesNecessaryIngredients[s].name}\n : list of ingredients : ${arrayRecipesNecessaryIngredients[s].listOfIngredients}\n`
 }
-console.log(`The name recipes which include necessary ingredients : \n ${listNameNecessaryIngredients}`)
-
+console.log(`The name recipes which include necessary ingredients : картопля,морква\n ${listNameNecessaryIngredients}`)
